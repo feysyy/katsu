c9a2a3221dfd71c1a9034dc907f0f4ecac8cf0a7
feysyy <feysy97@gmail.com>
2023-02-20 15:52:37 +0800

    Add columns to manga table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_20_073605) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_20_074906) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_20_073605) do
     t.bigint "collection_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
+    t.string "poster"
+    t.string "description"
+    t.string "status"
+    t.string "startDate"
+    t.float "episodes"
     t.index ["collection_id"], name: "index_mangas_on_collection_id"
   end
 
