d84679f9cc0e2b3818ed96b4bd3bd8a697a17d7e
feysyy <feysy97@gmail.com>
2023-03-01 23:52:53 +0800

    Add logic to save anime id to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,14 @@
 class Post < ApplicationRecord
   belongs_to :user
-  belongs_to :anime
-  belongs_to :manga
-  has_many :comments
+  
+  before_commit :save_anime_id, on: [:create]
+# 
+  private
+
+  def save_anime_id
+    response = Kitsu::Api.fetch_single_anime(anime_id)
+    anime = response['data']
+
+    self.anime_id = anime['id']
+  end
 end
